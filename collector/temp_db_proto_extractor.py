e445089d11de05397f051ac97e8259928d4122d5
Peter Gwinn <pmgwinn@users.noreply.github.com>
2014-10-15 12:37:01 -0700

    Clean up trailing whitespace

--- collector/temp_db_proto_extractor.py
+++ collector/temp_db_proto_extractor.py
@@ -25,12 +25,12 @@ def extract_all(db_file):
     protos = conn.execute('''
         SELECT
           rowid,
-          timestamp, 
-          temp_degrees_c, 
-          relative_humidity, 
+          timestamp,
+          temp_degrees_c,
+          relative_humidity,
           sensor,
           debug
-        FROM 
+        FROM
           temp_and_humidity
         WHERE
           NOT uploaded;''').fetchall()
@@ -40,8 +40,8 @@ def extract_all(db_file):
 def update_uploaded(db_file, ids):
   with sqlite3.connect(db_file) as conn:
     c = conn.execute('''
-        UPDATE temp_and_humidity 
-        SET uploaded = 1 
+        UPDATE temp_and_humidity
+        SET uploaded = 1
         WHERE rowid IN (%s)''' % ', '.join(map(str, ids)))
     return c.rowcount
 
@@ -51,22 +51,23 @@ def main():
       description='Upload sensor data from a database to the cloud.')
   parser.add_argument('--remote_host', type=str, default='localhost:8080',
       help='Host of the upload service.')
-  parser.add_argument('--db_file', type=str, 
+  parser.add_argument('--db_file', type=str,
       default=os.path.join(os.path.dirname(__file__), 'temp.db'),
       help='sqlite database with records to upload.')
   args = parser.parse_args()
-  
+
   records = extract_all(args.db_file)
   num_sent = len(records)
   request_proto = pb.UploadRequest()
   request_proto.temp_and_humidty_data.extend(records.values())
-  
+
   url = 'http://%s/upload' % args.remote_host
   resp = requests.post(url, data=request_proto.SerializeToString())
-  
+
   response_proto = pb.UploadResponse()
   response_proto.ParseFromString(resp.content)
-  print 'Sent up %d records, response verified %d were received' % (num_sent, response_proto.num_saved)
+  print ('Sent up %d records, response verified %d were received'
+      % (num_sent, response_proto.num_saved))
   num_updated = update_uploaded(args.db_file, records.keys())
   print '%d records marked uploaded' % num_updated
 
