26e898e605ea1ac44064895d1cd4e866d8cc7ff8
Peter Gwinn <pmgwinn@users.noreply.github.com>
2015-04-05 23:26:40 -0700

    run through go fmt

--- appengine/temp-sensor-app/app.go
+++ appengine/temp-sensor-app/app.go
@@ -1,160 +1,160 @@
 package temp_sensor_app
 
 import (
-  "errors"
-  "fmt"
-  "io/ioutil"
-  "net/http"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
-  "code.google.com/p/goprotobuf/proto"
+	"code.google.com/p/goprotobuf/proto"
 
-  "appengine"
-  "appengine/datastore"
+	"appengine"
+	"appengine/datastore"
 
-  "htu21df"
+	"htu21df"
 )
 
 const EntityName = "TempAndHumidity"
 
 type tempAndHumidityRecord struct {
-  ClientId string // not clear this is needed after upload confirmed.
-  RecordedTimestampMs int64
-  TempDegreesC float64
-  PercentRelativeHumidity float64
-  SensorName string `datastore:",noindex"`
-  Debug string `datastore:",noindex"`
+	ClientId                string // not clear this is needed after upload confirmed.
+	RecordedTimestampMs     int64
+	TempDegreesC            float64
+	PercentRelativeHumidity float64
+	SensorName              string `datastore:",noindex"`
+	Debug                   string `datastore:",noindex"`
 }
 
 func init() {
-  http.HandleFunc("/", root)
-  http.HandleFunc("/upload", upload)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/upload", upload)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "Temp and humidity.")
+	fmt.Fprint(w, "Temp and humidity.")
 }
 
 func respondWith400(w http.ResponseWriter, c appengine.Context, err error, msg string) {
-  w.WriteHeader(http.StatusBadRequest)
-  c.Errorf("%v error: %v", msg, err.Error())
+	w.WriteHeader(http.StatusBadRequest)
+	c.Errorf("%v error: %v", msg, err.Error())
 }
 
 func respondWith500(w http.ResponseWriter, c appengine.Context, err error, msg string) {
-  w.WriteHeader(http.StatusInternalServerError)
-  c.Errorf("%v error: %v", msg, err.Error())
+	w.WriteHeader(http.StatusInternalServerError)
+	c.Errorf("%v error: %v", msg, err.Error())
 }
 
 func checkErr(w http.ResponseWriter, c appengine.Context, err error, msg string) bool {
-  if err != nil {
-    respondWith500(w, c, err, msg)
-    return true
-  }
-  return false
+	if err != nil {
+		respondWith500(w, c, err, msg)
+		return true
+	}
+	return false
 }
 
 func newRecordKey(c appengine.Context) *datastore.Key {
 	key := datastore.NewIncompleteKey(c, EntityName, nil)
-  return key
+	return key
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
-  c := appengine.NewContext(r)
-  c.Infof("Requested URL: %v", r.URL)
+	c := appengine.NewContext(r)
+	c.Infof("Requested URL: %v", r.URL)
 
-  if (r.Body == nil) {
-    respondWith400(w, c, errors.New("Request body is nil"), "Request body is nil")
-  }
+	if r.Body == nil {
+		respondWith400(w, c, errors.New("Request body is nil"), "Request body is nil")
+	}
 
-  bytes, err := ioutil.ReadAll(r.Body)
-  if checkErr(w, c, err, "Failed to read request.") {
-    return
-  }
+	bytes, err := ioutil.ReadAll(r.Body)
+	if checkErr(w, c, err, "Failed to read request.") {
+		return
+	}
 
-  request := &htu21df.UploadRequest{}
-  err = proto.Unmarshal(bytes, request)
-  if checkErr(w, c, err, "Unable to parse proto.") {
-    return
-  }
+	request := &htu21df.UploadRequest{}
+	err = proto.Unmarshal(bytes, request)
+	if checkErr(w, c, err, "Unable to parse proto.") {
+		return
+	}
 
-  response := &htu21df.UploadResponse{}
-  if err = buildResponse(c, request, response); err != nil {
-    respondWith500(w, c, err, err.Error())
-    return
-  }
+	response := &htu21df.UploadResponse{}
+	if err = buildResponse(c, request, response); err != nil {
+		respondWith500(w, c, err, err.Error())
+		return
+	}
 
-  c.Infof("response %v", response.String())
-  responseData, err := proto.Marshal(response)
-  if checkErr(w, c, err, "Unable to encode response proto.") {
-    return
-  }
-  w.Write(responseData)
+	c.Infof("response %v", response.String())
+	responseData, err := proto.Marshal(response)
+	if checkErr(w, c, err, "Unable to encode response proto.") {
+		return
+	}
+	w.Write(responseData)
 }
 
 func buildResponse(c appengine.Context, request *htu21df.UploadRequest, response *htu21df.UploadResponse) error {
-  newKeys, err := saveRecords(c, request)
-  if err != nil {
-    return err
-  }
+	newKeys, err := saveRecords(c, request)
+	if err != nil {
+		return err
+	}
 
-  unsavedIds, err := verifySaved(c, request.DataAndClientId, newKeys)
-  if err != nil {
-    return err
-  }
+	unsavedIds, err := verifySaved(c, request.DataAndClientId, newKeys)
+	if err != nil {
+		return err
+	}
 
-  response.NumSaved = proto.Int(len(newKeys))
-  response.UnsavedClientIds = unsavedIds
-  return nil;
+	response.NumSaved = proto.Int(len(newKeys))
+	response.UnsavedClientIds = unsavedIds
+	return nil
 }
 
 func saveRecords(c appengine.Context, request *htu21df.UploadRequest) ([]*datastore.Key, error) {
-  numRecords := len(request.DataAndClientId)
-  c.Infof("request contains %v records", numRecords)
-  keys := make([]*datastore.Key, numRecords)
-  records := make([]*tempAndHumidityRecord, numRecords)
-  for i, dataAndId := range request.DataAndClientId {
-  	keys[i] = newRecordKey(c)
-  	records[i] = &tempAndHumidityRecord {
-      ClientId: dataAndId.GetClientId(),
-      RecordedTimestampMs: dataAndId.TempAndHumidtyData.GetRecordedTimestampMs(),
-      TempDegreesC: dataAndId.TempAndHumidtyData.GetTempDegreesC(),
-      PercentRelativeHumidity: dataAndId.TempAndHumidtyData.GetPercentRelativeHumidity(),
-      SensorName: dataAndId.TempAndHumidtyData.GetSensorName(),
-      Debug: dataAndId.TempAndHumidtyData.GetDebug(),
-    }
-  }
+	numRecords := len(request.DataAndClientId)
+	c.Infof("request contains %v records", numRecords)
+	keys := make([]*datastore.Key, numRecords)
+	records := make([]*tempAndHumidityRecord, numRecords)
+	for i, dataAndId := range request.DataAndClientId {
+		keys[i] = newRecordKey(c)
+		records[i] = &tempAndHumidityRecord{
+			ClientId:                dataAndId.GetClientId(),
+			RecordedTimestampMs:     dataAndId.TempAndHumidtyData.GetRecordedTimestampMs(),
+			TempDegreesC:            dataAndId.TempAndHumidtyData.GetTempDegreesC(),
+			PercentRelativeHumidity: dataAndId.TempAndHumidtyData.GetPercentRelativeHumidity(),
+			SensorName:              dataAndId.TempAndHumidtyData.GetSensorName(),
+			Debug:                   dataAndId.TempAndHumidtyData.GetDebug(),
+		}
+	}
 
-  newKeys, err := datastore.PutMulti(c, keys, records)
-  if err != nil {
-    return nil, err
-  }
-  return newKeys, nil
+	newKeys, err := datastore.PutMulti(c, keys, records)
+	if err != nil {
+		return nil, err
+	}
+	return newKeys, nil
 }
 
 func verifySaved(c appengine.Context, clientRecords []*htu21df.DataAndClientId, newKeys []*datastore.Key) ([]string, error) {
-  numSaved := len(newKeys)
-  numRecords := len(clientRecords)
+	numSaved := len(newKeys)
+	numRecords := len(clientRecords)
 
-  // TODO: handle case where numRecords > 0, but numSaved == 0?
-  //     should client look for IDs when none succeeded?
-  if numRecords != numSaved && numSaved > 0 {
-    // find unsaved client ids
-    unsavedIds := make([]string, numRecords - numSaved)
-    savedRecords := make([]*tempAndHumidityRecord, numSaved)
-    if err := datastore.GetMulti(c, newKeys, savedRecords); err != nil {
-      c.Infof("Unable to query saved records")
-      return nil, err
-    }
-    clientIds := make(map[string]bool)
-    for _, r := range savedRecords {
-      clientIds[r.ClientId] = true
-    }
-    for i, dataAndId := range clientRecords {
-      if !clientIds[dataAndId.GetClientId()] {
-        unsavedIds[i] = dataAndId.GetClientId()
-      }
-    }
-    c.Infof("Found unsaved ids %v", unsavedIds)
-    return unsavedIds, nil
-  }
-  return nil, nil
+	// TODO: handle case where numRecords > 0, but numSaved == 0?
+	//     should client look for IDs when none succeeded?
+	if numRecords != numSaved && numSaved > 0 {
+		// find unsaved client ids
+		unsavedIds := make([]string, numRecords-numSaved)
+		savedRecords := make([]*tempAndHumidityRecord, numSaved)
+		if err := datastore.GetMulti(c, newKeys, savedRecords); err != nil {
+			c.Infof("Unable to query saved records")
+			return nil, err
+		}
+		clientIds := make(map[string]bool)
+		for _, r := range savedRecords {
+			clientIds[r.ClientId] = true
+		}
+		for i, dataAndId := range clientRecords {
+			if !clientIds[dataAndId.GetClientId()] {
+				unsavedIds[i] = dataAndId.GetClientId()
+			}
+		}
+		c.Infof("Found unsaved ids %v", unsavedIds)
+		return unsavedIds, nil
+	}
+	return nil, nil
 }
--- appengine/temp-sensor-app/app_test.go
+++ appengine/temp-sensor-app/app_test.go
@@ -1,324 +1,322 @@
 package temp_sensor_app
 
 import (
-  "code.google.com/p/goprotobuf/proto"
+	"code.google.com/p/goprotobuf/proto"
 
-  "bytes"
-  "errors"
-  _ "fmt"
-  "io/ioutil"
-  _ "net/http"
-  "net/http/httptest"
-  "testing"
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http/httptest"
+	"testing"
 
-  "appengine"
-  "appengine/aetest"
-  "appengine/datastore"
+	"appengine"
+	"appengine/aetest"
+	"appengine/datastore"
 
-  "htu21df"
+	"htu21df"
 )
 
 type asserts struct {
-  t *testing.T
+	t *testing.T
 }
 
 func (a *asserts) assertStrsEqual(actual, expected string) {
-  if actual != expected {
-    a.t.Errorf("Got %v but expected %v", actual, expected)
-  }
+	if actual != expected {
+		a.t.Errorf("Got %v but expected %v", actual, expected)
+	}
 }
 
 func (a *asserts) assertIntsEqual(actual, expected int) {
-  if actual != expected {
-    a.t.Errorf("Got %v but expected %v", actual, expected)
-  }
+	if actual != expected {
+		a.t.Errorf("Got %v but expected %v", actual, expected)
+	}
 }
 
 func (a *asserts) failOnError(err error) {
-  if err != nil {
-   a.t.Fatal(err)
-  }
+	if err != nil {
+		a.t.Fatal(err)
+	}
 }
 
 func (a *asserts) IsTrue(test bool) {
-  if !test {
-   a.t.Fatal("Expected true value.")
-  }
+	if !test {
+		a.t.Fatal("Expected true value.")
+	}
 }
 
 func (a *asserts) IsFalse(test bool) {
-  if test {
-   a.t.Fatal("Expected false value.")
-  }
+	if test {
+		a.t.Fatal("Expected false value.")
+	}
 }
 
 func (a *asserts) IsNil(test interface{}) {
-  if test != nil {
-   a.t.Errorf("Expected nil value, but was %v.", test)
-  }
+	if test != nil {
+		a.t.Errorf("Expected nil value, but was %v.", test)
+	}
 }
 
 func newContext(t *testing.T) aetest.Context {
-  a := asserts{t}
+	a := asserts{t}
 
-  c, err := aetest.NewContext(nil)
-  a.failOnError(err)
-  return c
+	c, err := aetest.NewContext(nil)
+	a.failOnError(err)
+	return c
 }
 
 func TestRoot(t *testing.T) {
-  a := asserts{t}
-  w := httptest.NewRecorder()
-  root(w, nil)
-  a.assertStrsEqual(w.Body.String(), "Temp and humidity.")
+	a := asserts{t}
+	w := httptest.NewRecorder()
+	root(w, nil)
+	a.assertStrsEqual(w.Body.String(), "Temp and humidity.")
 }
 
 func TestRespondWith400(t *testing.T) {
-  a := asserts{t}
-  w := httptest.NewRecorder()
-  c := newContext(t)
-  defer c.Close()
+	a := asserts{t}
+	w := httptest.NewRecorder()
+	c := newContext(t)
+	defer c.Close()
 
-  respondWith400(w, c, errors.New("Test"), "Test Msg")
-  a.assertIntsEqual(w.Code, 400)
+	respondWith400(w, c, errors.New("Test"), "Test Msg")
+	a.assertIntsEqual(w.Code, 400)
 }
 
 func TestRespondWith500(t *testing.T) {
-  a := asserts{t}
-  w := httptest.NewRecorder()
-  c := newContext(t)
-  defer c.Close()
+	a := asserts{t}
+	w := httptest.NewRecorder()
+	c := newContext(t)
+	defer c.Close()
 
-  respondWith500(w, c, errors.New("Test"), "Test Msg")
-  a.assertIntsEqual(w.Code, 500)
+	respondWith500(w, c, errors.New("Test"), "Test Msg")
+	a.assertIntsEqual(w.Code, 500)
 }
 
 func TestCheckErr(t *testing.T) {
-  a := asserts{t}
+	a := asserts{t}
 
-  w := httptest.NewRecorder()
-  c := newContext(t)
-  defer c.Close()
+	w := httptest.NewRecorder()
+	c := newContext(t)
+	defer c.Close()
 
-  a.IsFalse(checkErr(w, c, nil, "No error"))
-  a.IsFalse(w.Code == 500)
+	a.IsFalse(checkErr(w, c, nil, "No error"))
+	a.IsFalse(w.Code == 500)
 
-  w = httptest.NewRecorder()
-  a.IsTrue(checkErr(w, c, errors.New("Error"), "Error!"))
-  a.assertIntsEqual(w.Code, 500)
+	w = httptest.NewRecorder()
+	a.IsTrue(checkErr(w, c, errors.New("Error"), "Error!"))
+	a.assertIntsEqual(w.Code, 500)
 }
 
 func TestNewRecordKey(t *testing.T) {
-  a := asserts{t}
+	a := asserts{t}
 
-  c := newContext(t)
-  defer c.Close()
+	c := newContext(t)
+	defer c.Close()
 
-  testKey := newRecordKey(appengine.Context(c))
-  a.assertStrsEqual(testKey.Kind(), EntityName)
-  //a.IsNil(testKey.Parent())
-  a.IsTrue(testKey.Incomplete())
+	testKey := newRecordKey(appengine.Context(c))
+	a.assertStrsEqual(testKey.Kind(), EntityName)
+	//a.IsNil(testKey.Parent())
+	a.IsTrue(testKey.Incomplete())
 }
 
 func TestSaveRecords(t *testing.T) {
-  a := asserts{t}
+	a := asserts{t}
 
-  c := newContext(t)
-  defer c.Close()
+	c := newContext(t)
+	defer c.Close()
 
-  newKeys, err := saveRecords(c, &htu21df.UploadRequest{})
-  a.IsNil(err)
-  a.assertIntsEqual(len(newKeys), 0)
+	newKeys, err := saveRecords(c, &htu21df.UploadRequest{})
+	a.IsNil(err)
+	a.assertIntsEqual(len(newKeys), 0)
 
-  req := htu21df.UploadRequest{
-    DataAndClientId: []*htu21df.DataAndClientId{
-      &htu21df.DataAndClientId{
-        ClientId: proto.String("client1"),
-        TempAndHumidtyData: &htu21df.TempAndHumidity{
-          RecordedTimestampMs: proto.Int64(1411103037254),
-          TempDegreesC: proto.Float64(23.95),
-          PercentRelativeHumidity: proto.Float64(68.78),
-          SensorName: proto.String("htu21df"),
-          Debug: proto.String("./temp_db_logger.py"),
-        },
-      },
-      &htu21df.DataAndClientId{
-        ClientId: proto.String("client2"),
-        TempAndHumidtyData: &htu21df.TempAndHumidity{
-          RecordedTimestampMs: proto.Int64(1411104501012),
-          TempDegreesC: proto.Float64(23.839),
-          PercentRelativeHumidity: proto.Float64(68.547),
-          SensorName: proto.String("htu21df"),
-          Debug: proto.String("./temp_db_logger.py"),
-        },
-      },
-      &htu21df.DataAndClientId{
-        ClientId: proto.String("client3"),
-        TempAndHumidtyData: &htu21df.TempAndHumidity{
-          RecordedTimestampMs: proto.Int64(1411104611852),
-          TempDegreesC: proto.Float64(23.828),
-          PercentRelativeHumidity: proto.Float64(68.615),
-          SensorName: proto.String("htu21df"),
-          Debug: proto.String("./temp_db_logger.py"),
-        },
-      },
-    },
-  }
+	req := htu21df.UploadRequest{
+		DataAndClientId: []*htu21df.DataAndClientId{
+			&htu21df.DataAndClientId{
+				ClientId: proto.String("client1"),
+				TempAndHumidtyData: &htu21df.TempAndHumidity{
+					RecordedTimestampMs:     proto.Int64(1411103037254),
+					TempDegreesC:            proto.Float64(23.95),
+					PercentRelativeHumidity: proto.Float64(68.78),
+					SensorName:              proto.String("htu21df"),
+					Debug:                   proto.String("./temp_db_logger.py"),
+				},
+			},
+			&htu21df.DataAndClientId{
+				ClientId: proto.String("client2"),
+				TempAndHumidtyData: &htu21df.TempAndHumidity{
+					RecordedTimestampMs:     proto.Int64(1411104501012),
+					TempDegreesC:            proto.Float64(23.839),
+					PercentRelativeHumidity: proto.Float64(68.547),
+					SensorName:              proto.String("htu21df"),
+					Debug:                   proto.String("./temp_db_logger.py"),
+				},
+			},
+			&htu21df.DataAndClientId{
+				ClientId: proto.String("client3"),
+				TempAndHumidtyData: &htu21df.TempAndHumidity{
+					RecordedTimestampMs:     proto.Int64(1411104611852),
+					TempDegreesC:            proto.Float64(23.828),
+					PercentRelativeHumidity: proto.Float64(68.615),
+					SensorName:              proto.String("htu21df"),
+					Debug:                   proto.String("./temp_db_logger.py"),
+				},
+			},
+		},
+	}
 
-  newKeys, err = saveRecords(c, &req)
-  a.IsNil(err)
-  a.assertIntsEqual(len(newKeys), 3)
+	newKeys, err = saveRecords(c, &req)
+	a.IsNil(err)
+	a.assertIntsEqual(len(newKeys), 3)
 
-  byClientId := make(map[string]*htu21df.TempAndHumidity)
-  for _, dataAndId := range req.DataAndClientId {
-    byClientId[*dataAndId.ClientId] = dataAndId.TempAndHumidtyData
-  }
+	byClientId := make(map[string]*htu21df.TempAndHumidity)
+	for _, dataAndId := range req.DataAndClientId {
+		byClientId[*dataAndId.ClientId] = dataAndId.TempAndHumidtyData
+	}
 
-  for _, newId := range newKeys {
-    rec := &tempAndHumidityRecord{}
-    err = datastore.Get(c, newId, rec)
-    a.IsNil(err)
+	for _, newId := range newKeys {
+		rec := &tempAndHumidityRecord{}
+		err = datastore.Get(c, newId, rec)
+		a.IsNil(err)
 
-    reqRec := byClientId[rec.ClientId]
-    a.IsTrue(rec.RecordedTimestampMs == *reqRec.RecordedTimestampMs)
-    a.IsTrue(rec.TempDegreesC == *reqRec.TempDegreesC)
-    a.IsTrue(rec.PercentRelativeHumidity == *reqRec.PercentRelativeHumidity)
-    a.assertStrsEqual(*reqRec.SensorName, rec.SensorName)
-    a.assertStrsEqual(*reqRec.Debug, rec.Debug)
-  }
+		reqRec := byClientId[rec.ClientId]
+		a.IsTrue(rec.RecordedTimestampMs == *reqRec.RecordedTimestampMs)
+		a.IsTrue(rec.TempDegreesC == *reqRec.TempDegreesC)
+		a.IsTrue(rec.PercentRelativeHumidity == *reqRec.PercentRelativeHumidity)
+		a.assertStrsEqual(*reqRec.SensorName, rec.SensorName)
+		a.assertStrsEqual(*reqRec.Debug, rec.Debug)
+	}
 }
 
 func TestVerifySaved(t *testing.T) {
-  a := asserts{t}
+	a := asserts{t}
 
-  c := newContext(t)
-  defer c.Close()
+	c := newContext(t)
+	defer c.Close()
 
-  // Test empty
-  missedIds, err := verifySaved(c, []*htu21df.DataAndClientId{}, []*datastore.Key{})
-  a.IsNil(err)
-  a.assertIntsEqual(len(missedIds), 0)
+	// Test empty
+	missedIds, err := verifySaved(c, []*htu21df.DataAndClientId{}, []*datastore.Key{})
+	a.IsNil(err)
+	a.assertIntsEqual(len(missedIds), 0)
 
-  // Test len's agree
-  missedIds, err = verifySaved(c,
-      []*htu21df.DataAndClientId{&htu21df.DataAndClientId{}, &htu21df.DataAndClientId{}},
-      []*datastore.Key{newRecordKey(c), newRecordKey(c)})
-  a.IsNil(err)
-  a.assertIntsEqual(len(missedIds), 0)
+	// Test len's agree
+	missedIds, err = verifySaved(c,
+		[]*htu21df.DataAndClientId{&htu21df.DataAndClientId{}, &htu21df.DataAndClientId{}},
+		[]*datastore.Key{newRecordKey(c), newRecordKey(c)})
+	a.IsNil(err)
+	a.assertIntsEqual(len(missedIds), 0)
 
-  // Test 3 requested, 1 saved
-  sentRecords := []*htu21df.DataAndClientId{
-    &htu21df.DataAndClientId{
-      ClientId: proto.String("client1"),
-      TempAndHumidtyData: &htu21df.TempAndHumidity{
-        RecordedTimestampMs: proto.Int64(1411103037254),
-        TempDegreesC: proto.Float64(23.95),
-        PercentRelativeHumidity: proto.Float64(68.78),
-        SensorName: proto.String("htu21df"),
-        Debug: proto.String("./temp_db_logger.py"),
-      },
-    },
-    &htu21df.DataAndClientId{
-      ClientId: proto.String("client2"),
-      TempAndHumidtyData: &htu21df.TempAndHumidity{
-        RecordedTimestampMs: proto.Int64(1411104501012),
-        TempDegreesC: proto.Float64(23.839),
-        PercentRelativeHumidity: proto.Float64(68.547),
-        SensorName: proto.String("htu21df"),
-        Debug: proto.String("./temp_db_logger.py"),
-      },
-    },
-    &htu21df.DataAndClientId{
-      ClientId: proto.String("client3"),
-      TempAndHumidtyData: &htu21df.TempAndHumidity{
-        RecordedTimestampMs: proto.Int64(1411104611852),
-        TempDegreesC: proto.Float64(23.828),
-        PercentRelativeHumidity: proto.Float64(68.615),
-        SensorName: proto.String("htu21df"),
-        Debug: proto.String("./temp_db_logger.py"),
-      },
-    },
-  }
+	// Test 3 requested, 1 saved
+	sentRecords := []*htu21df.DataAndClientId{
+		&htu21df.DataAndClientId{
+			ClientId: proto.String("client1"),
+			TempAndHumidtyData: &htu21df.TempAndHumidity{
+				RecordedTimestampMs:     proto.Int64(1411103037254),
+				TempDegreesC:            proto.Float64(23.95),
+				PercentRelativeHumidity: proto.Float64(68.78),
+				SensorName:              proto.String("htu21df"),
+				Debug:                   proto.String("./temp_db_logger.py"),
+			},
+		},
+		&htu21df.DataAndClientId{
+			ClientId: proto.String("client2"),
+			TempAndHumidtyData: &htu21df.TempAndHumidity{
+				RecordedTimestampMs:     proto.Int64(1411104501012),
+				TempDegreesC:            proto.Float64(23.839),
+				PercentRelativeHumidity: proto.Float64(68.547),
+				SensorName:              proto.String("htu21df"),
+				Debug:                   proto.String("./temp_db_logger.py"),
+			},
+		},
+		&htu21df.DataAndClientId{
+			ClientId: proto.String("client3"),
+			TempAndHumidtyData: &htu21df.TempAndHumidity{
+				RecordedTimestampMs:     proto.Int64(1411104611852),
+				TempDegreesC:            proto.Float64(23.828),
+				PercentRelativeHumidity: proto.Float64(68.615),
+				SensorName:              proto.String("htu21df"),
+				Debug:                   proto.String("./temp_db_logger.py"),
+			},
+		},
+	}
 
-  savedRec := &tempAndHumidityRecord {
-    ClientId: "client3",
-    RecordedTimestampMs: 1411104611852,
-    TempDegreesC: 23.828,
-    PercentRelativeHumidity: 68.615,
-    SensorName: "htu21df",
-    Debug: "./temp_db_logger.py",
-  }
-  savedKey, err := datastore.Put(c, newRecordKey(c), savedRec)
-  a.IsNil(err)
+	savedRec := &tempAndHumidityRecord{
+		ClientId:                "client3",
+		RecordedTimestampMs:     1411104611852,
+		TempDegreesC:            23.828,
+		PercentRelativeHumidity: 68.615,
+		SensorName:              "htu21df",
+		Debug:                   "./temp_db_logger.py",
+	}
+	savedKey, err := datastore.Put(c, newRecordKey(c), savedRec)
+	a.IsNil(err)
 
-  missedIds, err = verifySaved(c, sentRecords, []*datastore.Key{savedKey})
-  a.IsNil(err)
-  a.assertIntsEqual(len(missedIds), 2)
-  a.assertStrsEqual("client1", missedIds[0])
-  a.assertStrsEqual("client2", missedIds[1])
+	missedIds, err = verifySaved(c, sentRecords, []*datastore.Key{savedKey})
+	a.IsNil(err)
+	a.assertIntsEqual(len(missedIds), 2)
+	a.assertStrsEqual("client1", missedIds[0])
+	a.assertStrsEqual("client2", missedIds[1])
 }
 
 func TestUpload(t *testing.T) {
-  a := asserts{t}
-  inst, err := aetest.NewInstance(nil)
-  defer inst.Close()
-  a.IsNil(err)
+	a := asserts{t}
+	inst, err := aetest.NewInstance(nil)
+	defer inst.Close()
+	a.IsNil(err)
 
-  req := htu21df.UploadRequest{
-    DataAndClientId: []*htu21df.DataAndClientId{
-      &htu21df.DataAndClientId{
-        ClientId: proto.String("client1"),
-        TempAndHumidtyData: &htu21df.TempAndHumidity{
-          RecordedTimestampMs: proto.Int64(1411103037254),
-          TempDegreesC: proto.Float64(23.95),
-          PercentRelativeHumidity: proto.Float64(68.78),
-          SensorName: proto.String("htu21df"),
-          Debug: proto.String("./temp_db_logger.py"),
-        },
-      },
-      &htu21df.DataAndClientId{
-        ClientId: proto.String("client2"),
-        TempAndHumidtyData: &htu21df.TempAndHumidity{
-          RecordedTimestampMs: proto.Int64(1411104501012),
-          TempDegreesC: proto.Float64(23.839),
-          PercentRelativeHumidity: proto.Float64(68.547),
-          SensorName: proto.String("htu21df"),
-          Debug: proto.String("./temp_db_logger.py"),
-        },
-      },
-      &htu21df.DataAndClientId{
-        ClientId: proto.String("client3"),
-        TempAndHumidtyData: &htu21df.TempAndHumidity{
-          RecordedTimestampMs: proto.Int64(1411104611852),
-          TempDegreesC: proto.Float64(23.828),
-          PercentRelativeHumidity: proto.Float64(68.615),
-          SensorName: proto.String("htu21df"),
-          Debug: proto.String("./temp_db_logger.py"),
-        },
-      },
-    },
-  }
+	req := htu21df.UploadRequest{
+		DataAndClientId: []*htu21df.DataAndClientId{
+			&htu21df.DataAndClientId{
+				ClientId: proto.String("client1"),
+				TempAndHumidtyData: &htu21df.TempAndHumidity{
+					RecordedTimestampMs:     proto.Int64(1411103037254),
+					TempDegreesC:            proto.Float64(23.95),
+					PercentRelativeHumidity: proto.Float64(68.78),
+					SensorName:              proto.String("htu21df"),
+					Debug:                   proto.String("./temp_db_logger.py"),
+				},
+			},
+			&htu21df.DataAndClientId{
+				ClientId: proto.String("client2"),
+				TempAndHumidtyData: &htu21df.TempAndHumidity{
+					RecordedTimestampMs:     proto.Int64(1411104501012),
+					TempDegreesC:            proto.Float64(23.839),
+					PercentRelativeHumidity: proto.Float64(68.547),
+					SensorName:              proto.String("htu21df"),
+					Debug:                   proto.String("./temp_db_logger.py"),
+				},
+			},
+			&htu21df.DataAndClientId{
+				ClientId: proto.String("client3"),
+				TempAndHumidtyData: &htu21df.TempAndHumidity{
+					RecordedTimestampMs:     proto.Int64(1411104611852),
+					TempDegreesC:            proto.Float64(23.828),
+					PercentRelativeHumidity: proto.Float64(68.615),
+					SensorName:              proto.String("htu21df"),
+					Debug:                   proto.String("./temp_db_logger.py"),
+				},
+			},
+		},
+	}
 
-  protoBytes, err := proto.Marshal(&req)
-  a.IsNil(err)
+	protoBytes, err := proto.Marshal(&req)
+	a.IsNil(err)
 
-  reqBuf := bytes.NewBuffer(protoBytes)
-  httpReq, err := inst.NewRequest("POST", "/upload", reqBuf)
-  a.IsNil(err)
+	reqBuf := bytes.NewBuffer(protoBytes)
+	httpReq, err := inst.NewRequest("POST", "/upload", reqBuf)
+	a.IsNil(err)
 
-  w := httptest.NewRecorder()
-  upload(w, httpReq)
+	w := httptest.NewRecorder()
+	upload(w, httpReq)
 
-  respBytes, err := ioutil.ReadAll(w.Body)
-  a.IsNil(err)
-  response := &htu21df.UploadResponse{}
-  err = proto.Unmarshal(respBytes, response)
-  a.IsNil(err)
-  a.assertIntsEqual(3, int(*response.NumSaved))
+	respBytes, err := ioutil.ReadAll(w.Body)
+	a.IsNil(err)
+	response := &htu21df.UploadResponse{}
+	err = proto.Unmarshal(respBytes, response)
+	a.IsNil(err)
+	a.assertIntsEqual(3, int(*response.NumSaved))
 
-  c := appengine.NewContext(httpReq)
-  results := make([]*tempAndHumidityRecord, 3)
-  q := datastore.NewQuery(EntityName)
-  _, err = q.GetAll(c, results)
-  a.assertIntsEqual(3, len(results))
+	c := appengine.NewContext(httpReq)
+	results := make([]*tempAndHumidityRecord, 3)
+	q := datastore.NewQuery(EntityName)
+	_, err = q.GetAll(c, results)
+	a.assertIntsEqual(3, len(results))
 }
